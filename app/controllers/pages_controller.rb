e644320def1027f4ad746b5d0eeebb5a8316125d
Anton <panteleev23@gmail.com>
2021-03-06 20:33:29 +1100

    ARCH: solutions implemented. rounding uncommented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,23 +3,33 @@ class PagesController < ApplicationController
   end
 
   def sum
-    # @params = params['numbers'].split(",")
     @params = params['numbers'].scan(/\d+\.\d+|\d+/)
     sum = 0
     array = []
     @params.each do |number|
       sum += number.to_f
-      array.push(number)
     end
-    render json: sum #.round(2) # Remove for initial test
-    # render json: array
-
+    sum = sum.round(2)
+    # render json: sum #.round(2) # Remove for initial test
+    render json: sum
+# TODO LOGGER
     
   end
 
   def reverse_words
-    @params = params['sentence']
-    @params.reverse!
-    render json: @params
+    sentence = params['sentence']
+    new_sentence = ""
+    word = ""
+    sentence.each_char do |char|
+      if char.match?(/[a-zA-Z]/)
+        word += char
+      else
+        new_sentence += word.reverse! + char
+        word = ""
+      end 
+    end
+    new_sentence += word
+    # render json: sentence
+    render json: new_sentence
   end
 end
