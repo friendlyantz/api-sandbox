7fe66cd7a3ea0bda43c19d088abc4debab2bd11b
Anton <panteleev23@gmail.com>
2021-03-10 21:30:22 +1100

    TDD Implemented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,40 +2,15 @@ class PagesController < ApplicationController
   def hello
   end
 
-  # sum - non refactored
-    # def sum
-    #   Log.create(content: params['numbers'])
-    #   @params = number_params.split(",")
-    #   sum = 0
-    #   @params.each do |number|
-    #     sum += number.to_i
-    #     return render status: 400 if number.match?(/[^\d+|\-]/)
-    #   end
-    #       sum = sum.to_i
-    #       render json: sum
-    # end
-      
   def sum
     return render status: 400 if number_params.match?(/[^\d+\.\d+|\d+|\-|\,]/)
-    numbers_array = number_params.split(",").map(&:to_f)
-    result = numbers_array.sum.to_i
+    numbers_array = number_params.split(",").map(&:to_i)
+    result = numbers_array.sum
 
     render json: result
   end
-  # sum buffer
-    #   numbers_array = number_params.scan(/\d+\.\d+|\d+/)
-    #   Log.create(content: params['numbers'])
-    #   # Log.create(content: @params)
-    #   sum = 0
-    #   array = []
-    #   numbers_array.each do |number|
-    #     sum += number.to_f
-    #   end
-    #   sum = sum.round(2)
-    #   render json: sum
   
   def reverse_words
-    # sentence = params['sentence']
     sentence = sentence_params
 
     # sentence_array = sentence.split(/\b/)
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   get 'hello', to: 'pages#hello'
   get 'sum', to: 'pages#sum'
   get 'reverse-words', to: 'pages#reverse_words'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
